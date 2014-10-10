870a58f5daa553574fc7d6b4b02e17b1c3bb78bd
Xinyue Zhao <czhao@cellular.de>
2014-10-10 17:41:55 +0200

    Added crashlytics.

---
+++ app/src/main/java/widget/map/com/urlocationmapwidget/BootReceiver.java
@@ -0,0 +1,17 @@
+package widget.map.com.urlocationmapwidget;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Handling device boot by {@link android.content.BroadcastReceiver}.
+ *
+ * @author Xinyue Zhao
+ */
+public final class BootReceiver extends BroadcastReceiver {
+	@Override
+	public void onReceive(Context context, Intent intent) {
+	}
+}
+
