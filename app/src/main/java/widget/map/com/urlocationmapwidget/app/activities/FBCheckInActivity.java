e83e8e083e1717a456c9f703be3f642aeec24b92
XinyueZhao <zhao@eosnewmedia.de>
2014-12-02 13:49:03 +0100

    Added FB sdk and login logical.

--- app/src/main/java/widget/map/com/urlocationmapwidget/app/activities/FBCheckInActivity.java
+++ app/src/main/java/widget/map/com/urlocationmapwidget/app/activities/FBCheckInActivity.java
@@ -1,12 +1,26 @@
 package widget.map.com.urlocationmapwidget.app.activities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.TextView;
+
+import com.facebook.Request;
+import com.facebook.Response;
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.UiLifecycleHelper;
+import com.facebook.model.GraphUser;
+import com.facebook.widget.ProfilePictureView;
 
 import widget.map.com.urlocationmapwidget.R;
+import widget.map.com.urlocationmapwidget.utils.Prefs;
 
 /**
  * Show a dialog like activity before sharing on Facebook Inc.
@@ -19,6 +33,41 @@ public class FBCheckInActivity extends Activity {
 	 * Layout.
 	 */
 	private static final int LAYOUT = R.layout.activity_fbcheck_in;
+	/**
+	 * The FB component to keep Android live-cycle better.
+	 */
+	private UiLifecycleHelper mUiLifecycleHelper;
+	/**
+	 * The activity-code for returning from auth-activity of FB-SDK.
+	 */
+	private static final int REAUTH_ACTIVITY_CODE = 100;
+	/**
+	 * Some content of information of checkIn.
+	 */
+	private TextView mContentTv;
+	/**
+	 * Permissions for this app-FB-usage.
+	 */
+	private static final List<String> PERMISSIONS = new ArrayList<String>() {
+		{
+			add("public_profile");
+		}
+	};
+	/**
+	 * Show user's picture on Facebook Inc.
+	 */
+	private ProfilePictureView mUserPicIv;
+	/**
+	 * FB-Session listener.
+	 */
+	private Session.StatusCallback callback = new Session.StatusCallback() {
+		@Override
+		public void call(final Session session, final SessionState state, final Exception exception) {
+			if (session != null && session.isOpened()) {
+				makeUserInfoRequest(session);
+			}
+		}
+	};
 
 	/**
 	 * Show single instance of {@link FBCheckInActivity}
@@ -36,6 +85,33 @@ public class FBCheckInActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(LAYOUT);
+		mContentTv = (TextView) findViewById(R.id.check_in_content_tv);
+		mUserPicIv = (ProfilePictureView) findViewById(R.id.selection_profile_pic);
+		mUserPicIv.setCropped(true);
+		mUiLifecycleHelper = new UiLifecycleHelper(this, callback);
+		mUiLifecycleHelper.onCreate(savedInstanceState);
+
+		//Show a user name to confirm that FB connection has been established.
+		Session session = Session.getActiveSession();
+		if (session == null ||
+				!session.isOpened()) {
+			Session.openActiveSession(
+					this,
+					true,
+					PERMISSIONS,
+					new Session.StatusCallback() {
+						@Override
+						public void call(Session session, SessionState state, Exception exception) {
+							if(state == SessionState.CLOSED_LOGIN_FAILED) {
+								finish();
+							} else {
+								makeUserInfoRequest(session);
+							}
+						}
+					});
+		} else {
+			makeUserInfoRequest(session);
+		}
 	}
 
 	/**
@@ -56,10 +132,67 @@ public class FBCheckInActivity extends Activity {
 	 */
 	public void confirm(View view) {
 		//TODO share and checkIn on the Facebook Inc.
+		Prefs prefs = Prefs.getInstance(getApplication());
+		String latlng =  prefs.getLastLocation();
+
+		if(TextUtils.isEmpty(latlng)) {
+			return;
+		}
+		String[] latlngs = latlng.split(",");
+		Double.parseDouble(latlngs[0]);
+		Double.parseDouble(latlngs[1]);
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		mUiLifecycleHelper.onResume();
+	}
+
+	@Override
+	public void onSaveInstanceState(Bundle bundle) {
+		super.onSaveInstanceState(bundle);
+		mUiLifecycleHelper.onSaveInstanceState(bundle);
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+		mUiLifecycleHelper.onPause();
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		mUiLifecycleHelper.onDestroy();
 	}
 
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
+		Session.getActiveSession().onActivityResult(this, requestCode, resultCode, data);
+		mUiLifecycleHelper.onActivityResult(requestCode, resultCode, data);
+	}
+
+	/**
+	 * A FB-Request to get user information.
+	 * @param session A FB-session.
+	 */
+	private void makeUserInfoRequest(final Session session) {
+		Request request = Request.newMeRequest(session, new Request.GraphUserCallback() {
+					@Override
+					public void onCompleted(GraphUser user, Response response) {
+						// If the response is successful
+						if (session == Session.getActiveSession()) {
+							if (user != null) {
+								String info = String.format(getString(R.string.lbl_confirm_check_in), user.getName());
+								mContentTv.setVisibility(View.VISIBLE);
+								mContentTv.setText(info);
+								mUserPicIv.setProfileId(user.getId());
+							}
+						}
+					}
+				});
+		request.executeAsync();
 	}
 }
